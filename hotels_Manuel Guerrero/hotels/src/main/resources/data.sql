475cd211cf42f392e87da2343283153c59759974
Guerrero2403 <134355177+Guerrero2403@users.noreply.github.com>
2024-07-03 19:30:11 -0500

    Add files via upload

---
+++ hotels_Manuel Guerrero/hotels/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO hotel(hotel_id, hotel_name, hotel_address)
+VALUES (1, 'Hotel Ibis', 'Santiago Oriente 342');
+
+INSERT INTO hotel(hotel_id, hotel_name, hotel_address)
+VALUES (2, 'Hotel Transilvania', 'Calle transilvania 444');
